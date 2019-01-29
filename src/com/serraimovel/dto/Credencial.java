5b657c729306ff78bc26092138ef6c835c08ea79
Smythy Costa <smythy.costa@hotmail.com>
2019-01-29 15:04:49 -0300

    class dto Credencial

---
+++ src/com/serraimovel/dto/Credencial.java
@@ -0,0 +1,29 @@
+package com.serraimovel.dto;
+
+public class Credencial {
+	
+	private String usuario;
+	private String senha;
+
+	public Credencial() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+}
