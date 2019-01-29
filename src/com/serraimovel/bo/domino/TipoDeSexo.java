4cfcf29e3f3d284c9fd6e597c2b7b1cd4f9c5584
Smythy Costa <smythy.costa@hotmail.com>
2019-01-29 11:50:57 -0300

    Dominio TipoDeSexo

---
+++ src/com/serraimovel/bo/domino/TipoDeSexo.java
@@ -0,0 +1,38 @@
+package com.serraimovel.bo.domino;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum TipoDeSexo {
+
+	FEMININO(1, "Feminino"), MASCULINO(2, "Masculino");
+
+	private final int valor;
+	private final String nome;
+
+	private static final Map<Integer, TipoDeSexo> intToEnum = new HashMap<Integer, TipoDeSexo>();
+
+	TipoDeSexo(int valor, String nome) {
+		this.valor = valor;
+		this.nome = nome;
+	}
+
+	static {
+		for (TipoDeSexo tipo : values()) {
+			intToEnum.put(tipo.valor, tipo);
+		}
+	}
+
+	public int getValor() {
+		return valor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public static TipoDeSexo fromInt(int valor) {
+		return intToEnum.get(valor);
+	}
+
+}
