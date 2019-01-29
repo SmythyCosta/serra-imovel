5b083c4d5e7474f87fc8e09f97cc94ba55d95660
Smythy Costa <smythy.costa@hotmail.com>
2019-01-29 11:47:22 -0300

    Dominio TipoDeEmail

---
+++ src/com/serraimovel/bo/domino/TipoDeEmail.java
@@ -0,0 +1,39 @@
+package com.serraimovel.bo.domino;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum TipoDeEmail {
+
+	PESSOAL(1, "Pessoal"), COMERCIAL(2, "Comercial");
+
+	private final int valor;
+	private final String nome;
+
+	private static final Map<Integer, TipoDeEmail> intToEnum = 
+		new HashMap<Integer, TipoDeEmail>();
+
+	TipoDeEmail(int valor, String nome) {
+		this.valor = valor;
+		this.nome = nome;
+	}
+
+	static {
+		for (TipoDeEmail tipo : values()) {
+			intToEnum.put(tipo.valor, tipo);
+		}
+	}
+
+	public int getValor() {
+		return valor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public static TipoDeEmail fromInt(int valor) {
+		return intToEnum.get(valor);
+	}
+
+}
