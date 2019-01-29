c4ee624c6105cbd83a579708c38ff6992794c999
Smythy Costa <smythy.costa@hotmail.com>
2019-01-29 17:28:49 -0300

    cmd CmdListarFuncionarios

---
+++ src/com/serraimovel/command/CmdListarFuncionarios.java
@@ -0,0 +1,39 @@
+package com.serraimovel.command;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpSession;
+
+import com.serraimovel.dto.PessoaFisica;
+import com.serraimovel.excecao.SerraImovelException;
+
+public class CmdListarFuncionarios extends CmdAbstrato {
+
+	@Override
+	public void processar() throws ServletException, IOException {
+
+		String url = null;
+		try {
+		
+			FuncionarioBO bo = new FuncionarioBO();
+			
+			List<PessoaFisica> lstFuncionarios = bo.buscarFuncionarios();
+			
+			HttpSession sessao = request.getSession();
+			
+			sessao.setAttribute("funcionarios", lstFuncionarios);
+			
+			url = "/jsp/listarFuncionarios.jsp";
+		
+		} catch (SerraImovelException exc) {
+			request.setAttribute("excecao", exc);
+			url = "/jsp/errorPage.jsp";
+		}
+		
+		forward(url);
+
+	}
+
+}
