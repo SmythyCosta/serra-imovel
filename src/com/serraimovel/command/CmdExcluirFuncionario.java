2e75127c637ee16c7e30931569d8aecc9f48769b
Smythy Costa <smythy.costa@hotmail.com>
2019-01-31 07:29:06 -0300

    finalized cmd CmdExcluirFuncionario

--- src/com/serraimovel/command/CmdExcluirFuncionario.java
+++ src/com/serraimovel/command/CmdExcluirFuncionario.java
@@ -1,5 +1,42 @@
 package com.serraimovel.command;
 
-public class CmdExcluirFuncionario {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+
+import com.serraimovel.bo.FuncionarioBO;
+import com.serraimovel.excecao.SerraImovelException;
+
+public class CmdExcluirFuncionario extends CmdAbstrato{
+
+	@Override
+	public void processar() throws ServletException, IOException {
+
+		String url = null;
+		try {
+		
+			FuncionarioBO bo = new FuncionarioBO();
+			
+			Integer func = Integer.parseInt(request.getParameter("id_funcionario"));
+						
+			Integer funcionario = bo.excluirPorFuncionario(func);
+						
+			//HttpSession seas = request.getSession();
+			
+			//sessao.setAttribute("funcionario", funcionario);
+			
+			request.setAttribute("funcionario", funcionario);
+			
+			url = "controller?cmd=ListarFuncionarios";
+		
+		} catch (SerraImovelException exc) {
+			request.setAttribute("excecao", exc);
+			url = "/jsp/errorPage.jsp";
+		} finally {
+		}
+		
+		forward(url);
+
+	}
+	
 }
